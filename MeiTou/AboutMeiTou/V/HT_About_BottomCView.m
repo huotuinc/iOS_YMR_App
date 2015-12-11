7dad259bc2e653d9bf817273d544be7968c26b23
che <BigChe777@163.com>
2015-12-11 16:26:09 +0800

    123

---
+++ MeiTou/AboutMeiTou/V/HT_About_BottomCView.m
@@ -0,0 +1,23 @@
+//
+//  HT_About_BottomCView.m
+//  MeiTou
+//
+//  Created by che on 15/12/11.
+//  Copyright © 2015年 车. All rights reserved.
+//
+
+#import "HT_About_BottomCView.h"
+
+@implementation HT_About_BottomCView
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
